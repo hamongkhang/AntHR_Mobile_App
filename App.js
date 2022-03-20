2c482b321c7144991e44ab5f27b9b23e88b10231
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-20 10:12:37 +0700

    create project

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {NavigationContainer} from '@react-navigation/native';
+import Welcome from "./Screen/Welcom";
+import SplashScreen from './Screen/SplashScreen';
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+     <NavigationContainer>
+       <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{headerShown: false}}
+        /> 
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        /> 
+         <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        /> 
+       <Stack.Screen
+          name="DrawerNavigationRoutes"
+          component={DrawerNavigationRoutes}
+          Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        />  
+     </Stack.Navigator>
+     </NavigationContainer>
+  );
+};
+
+export default App;

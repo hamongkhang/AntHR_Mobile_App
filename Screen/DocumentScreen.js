51b0cd9d8f9771004f76163887e4b42d7f1a592b
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-23 13:01:44 +0700

    create:end documents features

--- Screen/DocumentScreen.js
+++ Screen/DocumentScreen.js
@@ -15,23 +15,65 @@ const DocumentScreen = ({ navigation }) => {
   const [expanded, setExpanded] = React.useState(false);
   const [news, setNews] = useState([]);
   const [check, setCheck] = useState(false);
+  const [check2, setCheck2] = useState(false);
+  const [result2, setResult2] = useState([]);
+  const [searchQuery2, setSearchQuery2] = React.useState('');
+
   const [result, setResult] = useState([]);
   const [detail, setDetail] = useState([]);
-  const [checkDetail, setCheckDetail] = useState(true);
+  const [checkDocuments, setCheckDocuments] = useState(true);
   const [folders, setFolders] = useState([]);
-  //   const getNews = (token) =>{
-  //     setLoading(true);
-  //     fetch(REACT_APP_API+'/new/getAllNew', {
-  //         method: "GET",
-  //         headers: {"Authorization": `Bearer `+token}
-  //       })
-  //     .then(response => response.json())
-  //     .then(data =>  {
-  //         setLoading(false);
-  //         setNews(data.data);
-  //     });
-  //   }
+  const [token, setToken] = useState('');
+  const [documents, setDocuments] = useState([]);
+  const [page, setPage] = React.useState(0);
+  const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
 
+  const getOneDocumentFolders = (id) =>{
+    setLoading(true);
+    setCheckDocuments()
+    fetch(REACT_APP_API+'/document/getOneFolder/'+id, {
+        method: "GET",
+        headers: {"Authorization": `Bearer `+token}
+      })
+    .then(response => response.json())
+    .then(data =>  {
+        setLoading(false);
+        setDocuments(data.data);
+    });
+}
+console.log(result2)
+const onChangeSearch2 = query => {
+  setSearchQuery2(query);
+  setCheck2(true);
+  var a = [];
+  for (var i = 0; i < documents.length; i++) {
+    if (documents[i].name_show.indexOf(query) != -1) {
+      a.push(documents[i]);
+    } else {
+      setResult2([]);
+    }
+  }
+  setResult2(a);
+
+}
+
+
+
+
+
+
+  const getFolder = (token) => {
+    setLoading(true);
+    fetch(REACT_APP_API + '/document/getAllFolder', {
+      method: "GET",
+      headers: { "Authorization": `Bearer ` + token }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setLoading(false);
+        setFolders(data.data);
+      });
+  }
   const onChangeSearch = query => {
     setSearchQuery(query);
     setCheck(true);
@@ -46,61 +88,14 @@ const DocumentScreen = ({ navigation }) => {
     setResult(a);
 
   }
+ 
 
-  //   const onChangeSelect = (value) => {
-  //     setCheck(true);
-  //     setSelect(value);
-  //     setExpanded(!expanded);
-  //     var a=[];
-  //    if(value=="Important"){
-  //     for (var i = 0; i < news.length; i++) {
-  //       if (news[i].important == 1) {
-  //         a.push(news[i]);
-  //       }
-  //     }
-  //   }else if(value=="Non Important"){
-  //     for (var i = 0; i < news.length; i++) {
-  //       if (news[i].important == 0) {
-  //         a.push(news[i]);
-  //       }
-  //     }
-  //   }else{
-  //     for (var i = 0; i < news.length; i++) {
-  //        a.push(news[i])
-  //     }
-  //   }
-  //   setResult(a)
-  //   }   
-
-  //   const onChangeNewDetail=(id)=>{
-  //     setCheckDetail(false);
-  //     var a=[];
-  //     for (var i = 0; i < news.length; i++) {
-  //       if (news[i].id == id) {
-  //         a.push(news[i]);
-  //       }
-  //     }
-  //     setDetail(a);
-  //   }
-  const getFolder = (token) => {
-    setLoading(true);
-    fetch(REACT_APP_API + '/document/getAllFolder', {
-      method: "GET",
-      headers: { "Authorization": `Bearer ` + token }
-    })
-      .then(response => response.json())
-      .then(data => {
-        setLoading(false);
-        setFolders(data.data);
-      });
-  }
-  const [page, setPage] = React.useState(0);
-  const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
 
   useEffect(() => {
     const getToken = async () => {
       try {
         const token = await AsyncStorage.getItem("access_token");
+        setToken(token);
         getFolder(token);
       } catch (err) {
         console.log(err);
@@ -110,7 +105,7 @@ const DocumentScreen = ({ navigation }) => {
     setPage(0);
   }, [itemsPerPage]);
   return (
-    // checkDocument?
+     checkDocuments?
     <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
       <View style={styles.mainBody}>
         <Loader loading={loading} />
@@ -139,7 +134,7 @@ const DocumentScreen = ({ navigation }) => {
                     if (item.share == 1) {
                       return (
                         <DataTable.Row>
-                          <DataTable.Cell>{item.name ? item.name : "-"}</DataTable.Cell>
+                          <DataTable.Cell onPress={()=>getOneDocumentFolders(item.id)}>{item.name ? item.name : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.author ? item.author : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.description ? item.description : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.sum ? item.sum : "-"}</DataTable.Cell>
@@ -156,7 +151,7 @@ const DocumentScreen = ({ navigation }) => {
                   if (item.share == 1) {
                   return (
                     <DataTable.Row>
-                          <DataTable.Cell>{item.name ? item.name : "-"}</DataTable.Cell>
+                          <DataTable.Cell onPress={()=>getOneDocumentFolders(item.id)}>{item.name ? item.name : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.author ? item.author : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.description ? item.description : "-"}</DataTable.Cell>
                           <DataTable.Cell>{item.sum ? item.sum : "-"}</DataTable.Cell>
@@ -188,51 +183,88 @@ const DocumentScreen = ({ navigation }) => {
 
       </View>
     </LinearGradient>
-    // :
-    // <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
-    //   <View style={styles.mainBodyDetail}>
-    //     <View style={styles.item_one_detail}>
-    //     <View style={{flexDirection:"row",alignItems:"center",marginBottom:40}}>
-    //       <Image
-    //                 source={require('../Image/back_icon.png')}
-    //                 style={{
-    //                   width: 15,
-    //                   height: 15,
-    //                   marginRight:5
-    //                 }}
-    //         />         
-    //         <Text style={{fontSize:14,fontHeight:20,color:"rgb(79, 94, 113)",fontWeight:"bold",marginLeft:5}} onPress={()=>setCheckDetail(true)}>Back</Text>  
-    //     </View>
-    //       <View style={{flexDirection:"row",alignItems:"center"}}>
-    //               <Text  style={{fontSize:18,fontHeight:20,color:"rgb(35, 54, 78)",fontWeight:"bold"}}>
-    //                 {detail[0].title?detail[0].title:"-"}
-    //               </Text>
-    //               {(detail[0].important==1)
-    //               ?
-    //               <Text style={{fontSize:17,paddingTop:4,paddingBottom:4,paddingRight:7,paddingLeft:7,backgroundColor:"rgb(216, 246, 226)",color:"rgb(31, 153, 70)",borderRadius:3,marginLeft:10}}>IMPORTANT</Text>
-    //               :
-    //               null}
-    //             </View>
-    //             <View style={{flexDirection:"row",alignItems:"center"}}>
-    //               <Image
-    //                 source={require('../Image/clock_icon.png')}
-    //                 style={{
-    //                   width: 14,
-    //                   height: 14,
-    //                   marginRight:5
-    //                 }}
-    //               />                  
-    //               <Text style={{fontWeight:500,fontSize:16,color:"rgb(105, 129, 148)",marginRight:5}}>    
-    //                                             {new Intl.DateTimeFormat('de-DE', { 
-    //                                                 year: 'numeric', month: 'long', day: 'numeric' 
-    //                                             }).format(new Date(detail[0].updated_at))} | Admin</Text>
-    //             </View>
-    //             <View style={{flexDirection:"row",alignItems:"center",marginTop:50}}>
-    //               <Text style={{color:"rgb(14, 34, 61)"}}>{detail[0].content?detail[0].content:"-"}</Text>
-    //             </View>
-    //     </View>
-    //   </View>
-    // </LinearGradient>
+    :
+    <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      
+      <View style={styles.header}>
+      <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 40 }}>
+              <Image
+                source={require('../Image/back_icon.png')}
+                style={{
+                  width: 15,
+                  height: 15,
+                  marginRight: 5
+                }}
+              />
+              <Text style={{ fontSize: 14, fontHeight: 20, color: "rgb(79, 94, 113)", fontWeight: "bold", marginLeft: 5 }} onPress={() => setCheckDocuments(true)}>Back</Text>
+            </View>
+        <Searchbar
+          style={{ borderColor: "rgb(212, 223, 231)", borderWidth: 1, boxShadow: "rgb(0 0 0 / 24%) 0px 3px 4px", height: 60, marginBottom: 10 }}
+          placeholder="Search"
+          onChangeText={onChangeSearch2}
+          value={searchQuery2}
+        />
+      </View>
+      <View style={styles.item_one}>
+
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Name</DataTable.Title>
+            <DataTable.Title>Size</DataTable.Title>
+            <DataTable.Title>Created At</DataTable.Title>
+          </DataTable.Header>
+          <ScrollView>
+            {!check2 ?
+              documents.length ?
+              documents.map((item, index) => {
+                    return (
+                      <DataTable.Row>
+                        <DataTable.Cell>{item.name_show ? item.name_show : "-"}</DataTable.Cell>
+                        <DataTable.Cell>{item.size ? item.size : "-"}</DataTable.Cell>
+                        <DataTable.Cell>
+                          {new Intl.DateTimeFormat('de-DE', {
+                            year: 'numeric', month: 'long', day: 'numeric'
+                          }).format(new Date(item.updated_at))}
+                        </DataTable.Cell>
+                      </DataTable.Row>
+                    )
+                  
+                }) : null
+              : result2.length ? result2.map((item, index) => {
+                return (
+                  <DataTable.Row>
+                  <DataTable.Cell>{item.name_show ? item.name_show : "-"}</DataTable.Cell>
+                  <DataTable.Cell>{item.size ? item.size : "-"}</DataTable.Cell>
+                  <DataTable.Cell>
+                    {new Intl.DateTimeFormat('de-DE', {
+                      year: 'numeric', month: 'long', day: 'numeric'
+                    }).format(new Date(item.updated_at))}
+                  </DataTable.Cell>
+                </DataTable.Row>
+                )
+              }) :
+                null
+            }
+
+          </ScrollView>
+          <DataTable.Pagination
+            page={page}
+            numberOfPages={4}
+            onPageChange={(page) => setPage(page)}
+            label="1-2 of 6"
+            optionsPerPage={optionsPerPage}
+            itemsPerPage={itemsPerPage}
+            setItemsPerPage={setItemsPerPage}
+            showFastPagination
+            optionsLabel={'Rows per page'}
+          />
+        </DataTable>
+      </View>
+
+    </View>
+  </LinearGradient>
   );
 };
 export default DocumentScreen;

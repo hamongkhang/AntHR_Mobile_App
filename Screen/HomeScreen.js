790e17d39605f6724abdd8a0a33a68ae78e5ed03
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-24 13:43:22 +0700

    create: attendance with clients UI

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -75,7 +75,7 @@ const HomeScreen = ({ navigation }) => {
       <View style={{ flex: 1 }}>
         <Loader loading={loading} />
         <LinearGradient colors={['#312A6C', '#852D91']}>
-          <Appbar.Header style={{backgroundColor:"black"}}>
+          <Appbar.Header style={{backgroundColor:"rgb(14, 34, 61)"}}>
             <Image style={{ marginLeft: 10, width: 60, height: 60 }} source={require('../Image/logo1.png')} />
             <Text style={{ marginLeft: "auto", marginRight: 10 }} onPress={() => onClickShowButton()}>
               {
--- Screen/ScanScreen.js
+++ Screen/ScanScreen.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity, Image, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity, Image, Alert, ScrollView } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { LinearGradient } from "expo-linear-gradient";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { REACT_APP_API } from "@env"
 import Loader from './Loader';
-
 const ScanScreen = ({ navigation }) => {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
@@ -28,8 +27,6 @@ const ScanScreen = ({ navigation }) => {
                 setEmployees(data.data[1].reverse());
             });
     }
-    console.log(employees)
-    console.log(users)
 
     useEffect(() => {
         (async () => {
@@ -43,14 +40,14 @@ const ScanScreen = ({ navigation }) => {
     }, []);
     const handleBarCodeScanned = ({ type, data }) => {
         var a = {};
-        var kt=false;
+        var kt = false;
         for (var i = 0; i < employees.length; i++) {
             if (employees[i].user_id == data) {
-                kt=true;
+                kt = true;
                 a = employees[i];
             }
         }
-        if (kt==false) {
+        if (kt == false) {
             setScanned(true);
             Alert.alert("Unidentified Employee!",
                 `We are unable to verify employee information. Please scan again !`,
@@ -62,7 +59,7 @@ const ScanScreen = ({ navigation }) => {
         } else {
             setScanned(true);
             Alert.alert("Identified Employee !",
-                `${'First name: '+a.first_name+'\n'+'Last name: '+a.last_name+'\n'+'Email: '+a.email+'\n'+'Job: '+"Backend Developer"+'\n'+'Office: '+'Viet Nam'}`,
+                `${'First name: ' + a.first_name + '\n' + 'Last name: ' + a.last_name + '\n' + 'Email: ' + a.email + '\n' + 'Job: ' + "Backend Developer" + '\n' + 'Office: ' + 'Viet Nam'}`,
                 [
                     { text: 'Cancel' },
                 ],
@@ -72,7 +69,6 @@ const ScanScreen = ({ navigation }) => {
 
 
     };
-
     if (hasPermission === null) {
         return <Text style={{ color: "red", fontSize: 24, fontWeight: "bold", textAlign: "center", marginTop: "auto", marginBottom: "auto" }}>Requesting for camera permission</Text>;
     }
@@ -80,7 +76,7 @@ const ScanScreen = ({ navigation }) => {
         return <Text style={{ color: "red", fontSize: 24, fontWeight: "bold", textAlign: "center", marginTop: "auto", marginBottom: "auto" }}>No access to camera</Text>;
     }
     return (
-        (role == 0) ?
+        (role == 1) ?
             <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
                 <Loader loading={loading} />
                 {showCamera ?
@@ -97,8 +93,8 @@ const ScanScreen = ({ navigation }) => {
                                 <View style={styles.layerRight} />
                             </View>
                             <View style={styles.layerBottom}>
-                                <Text style={{ textAlign: "center", marginTop: "auto", fontWeight: "bold", fontSize: 40, color: "#ff9900"}}>AntHR</Text>
-                                <Text style={{ textAlign: "center", fontSize: 26 ,marginBottom:30}}>QR Code Scanner</Text>
+                                <Text style={{ textAlign: "center", marginTop: "auto", fontWeight: "bold", fontSize: 40, color: "#ff9900" }}>AntHR</Text>
+                                <Text style={{ textAlign: "center", fontSize: 26, marginBottom: 30 }}>QR Code Scanner</Text>
                             </View>
                         </BarCodeScanner>
                         {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} color="red" />}
@@ -118,7 +114,118 @@ const ScanScreen = ({ navigation }) => {
             </LinearGradient>
             :
             <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
-                <Text>Tôi không phải là admin</Text>
+                <Loader loading={loading} />
+                <View style={{ padding: 10 }}>
+                    <View style={{ padding: 10, paddingTop: 16, backgroundColor: "white", borderColor: "rgb(227, 235, 241)", borderWidth: 1, borderRadius: 5, paddingBottom: 16 }}>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <View style={{ backgroundColor: "#ff9900", borderRadius: "100%", marginRight: 10, marginTop: 1, padding: 6, justifyContent: "center", alignItems: "center" }}> <Image
+                                source={require('../Image/clock_icon.png')}
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                }}
+                            /></View>
+                            <Text style={{ fontSize: 18, color: "rgb(35, 54, 78)", lineHeight: 20, fontWeight: "bold" }}>My Attendance</Text>
+                        </View>
+                        <View style={{ backgroundColor: "#edf8f1", flexDirection: "row", borderRadius: 5, padding: 5, paddingTop: 10, paddingBottom: 10, marginTop: 10 }}>
+                            <Text style={{ fontSize: 14, fontWeight: "bold", color: "rgb(35, 54, 78)", lineHeight: 20, marginRight: 10 }}>Current Date: <Text style={{ fontSize: 12, fontWeight: "normal" }}>{new Date().toLocaleString()}</Text></Text>
+                            <Text style={{ fontSize: 14, fontWeight: "bold", color: "rgb(35, 54, 78)", lineHeight: 20 }}>Current Location: <Text style={{ fontSize: 12, fontWeight: "normal" }}>Viet Nam</Text></Text>
+                        </View>
+                        <View style={{ marginBottom: 20 }}>
+                            <Image
+                                source={require('../Image/qr_code.png')}
+                                style={{
+                                    flex: 1,
+                                    aspectRatio: 1.5,
+                                    resizeMode: 'contain',
+                                }}
+                            />
+                            <Text style={{ textAlign: "center", marginTop: 10, fontSize: 24, fontWeight: "bold", lineHeight: 20, color: "rgb(35, 54, 78)" }}>Scan me for Attendance</Text>
+                        </View>
+                        <ScrollView>
+                            <View style={{ borderRadius: 5, borderColor: "rgb(227, 235, 241)", borderWidth: 1, paddingTop: 20, marginBottom: 20 }}>
+                                <View style={{ flexDirection: "row", alignItems: "center", paddingLeft: 10, paddingRight: 10, paddingBottom: 10 }}>
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Date</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock In</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)", backgroundColor: "#edf8f1", padding: 10, borderRadius: 5 }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock In Location</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock Out</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)", backgroundColor: "#edf8f1", padding: 10, borderRadius: 5 }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock Out Location</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Work Schedule</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Status</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Note</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                            </View>
+                            <View style={{ borderRadius: 5, borderColor: "rgb(227, 235, 241)", borderWidth: 1, paddingTop: 20, marginBottom: 20 }}>
+                                <View style={{ flexDirection: "row", alignItems: "center", paddingLeft: 10, paddingRight: 10, paddingBottom: 10 }}>
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Date</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock In</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock In Location</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock Out</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Clock Out Location</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Work Schedule</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Status</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                                <View style={{ flexDirection: "row", alignItems: "center", padding: 10, borderTopColor: "rgb(227, 235, 241)", borderTopWidth: 0.2 }}>
+
+                                    <Text style={{ fontSize: 16, lineHeight: 20, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>Note</Text>
+                                    <Text style={{ marginLeft: "auto", fontSize: 16, lineHeight: 20, color: "rgb(14, 34, 61)" }}>{new Date().toLocaleString()}</Text>
+                                </View>
+                            </View>
+                        </ScrollView>
+                    </View>
+                </View>
             </LinearGradient>
     );
 }
@@ -134,21 +241,20 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#ff9900',
         alignItems: 'center',
-        borderRadius: 10,
+        borderRadius: 5,
         marginLeft: 35,
         marginRight: 35,
         marginTop: 20,
         marginBottom: 25,
-        paddingTop: 5,
-        paddingLeft: 15,
-        paddingBottom: 5,
+        paddingTop: 2,
+        paddingLeft: 20,
+        paddingBottom: 2,
         paddingRight: 15,
-        paddingLeft: 20
     },
     buttonTextStyle: {
         color: '#ff9900',
         paddingVertical: 10,
-        fontSize: 16,
+        fontSize: 24,
         fontWeight: "bold"
     },
     barCodeView: {
@@ -191,5 +297,5 @@ const styles = StyleSheet.create({
     layerBottom: {
         flex: 1,
         backgroundColor: "rgba(52, 52, 52, 0.8)",
-    },
+    }
 });

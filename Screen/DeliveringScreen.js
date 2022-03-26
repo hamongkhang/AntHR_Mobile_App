fae73047bdd083b1cb5102dba0422ed2ce2d1661
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-26 23:17:10 +0700

    fix: get data and create confirm with employees

--- Screen/DeliveringScreen.js
+++ Screen/DeliveringScreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, View, Text, ScrollView, Image, TouchableOpacity ,ToastAndroid } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image, TouchableOpacity, ToastAndroid } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Avatar,  FAB, Portal, Provider } from 'react-native-paper';
+import { Avatar, FAB, Portal, Provider } from 'react-native-paper';
 import { REACT_APP_API, REACT_APP_FILE } from "@env"
 import Loader from './Loader';
 import moment from 'moment';
@@ -18,120 +18,123 @@ const DeliveringScreen = ({ navigation }) => {
     const [lastName, setLastName] = useState('');
     const [firstName, setFirstName] = useState('');
     const [render, setRender] = useState(false);
-    const [orders, setOrders] =useState([]);
-    const getOrders=(token)=>{
+    const [orders, setOrders] = useState([]);
+    const getOrders = (token) => {
         setLoading(true);
-        fetch(REACT_APP_API+'/cart_present/getAllCartPresent', {
+        fetch(REACT_APP_API + '/cart_present/getAllCartPresent', {
             method: "GET",
-            headers: {"Authorization": `Bearer `+token}
-          })
-        .then(response => response.json())
-        .then(data =>  {
-              setOrders(data.data.reverse());
-              setLoading(false);
-        });
+            headers: { "Authorization": `Bearer ` + token }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setOrders(data.data.reverse());
+                setLoading(false);
+            });
     }
-    const onConfirmEmployee=(id)=>{
+    const onConfirmEmployee = (id) => {
         setLoading(true);
-        fetch(REACT_APP_API+'/cart_present/changeStatusClient/'+id, {
+        fetch(REACT_APP_API + '/cart_present/changeStatusClient/' + id, {
             method: "GET",
-            headers: {"Authorization": `Bearer `+token}
-          })
-          .then(response => response.json())
-          .then(data =>  {
-             if(data.error){
-                setLoading(false);
-                ToastAndroid.showWithGravityAndOffset('Public Failed !!!', ToastAndroid.LONG, ToastAndroid.CENTER, 10, 10);
-             }
-             else{
-                setLoading(false);
-                setRender(!render)
-                ToastAndroid.showWithGravityAndOffset('Public successfully !!!', ToastAndroid.LONG, ToastAndroid.CENTER, 10, 10);}
-          });
+            headers: { "Authorization": `Bearer ` + token }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    setLoading(false);
+                    ToastAndroid.showWithGravityAndOffset('Public Failed !!!', ToastAndroid.LONG, ToastAndroid.CENTER, 10, 10);
+                }
+                else {
+                    setLoading(false);
+                    setRender(!render)
+                    ToastAndroid.showWithGravityAndOffset('Public successfully !!!', ToastAndroid.LONG, ToastAndroid.CENTER, 10, 10);
+                }
+            });
     }
     useEffect(() => {
         const getToken = async () => {
-          try {
-            const token = await AsyncStorage.getItem("access_token");
-            const id_user = await AsyncStorage.getItem("id");
-            const avatar_user = await AsyncStorage.getItem("avatar");
-            const last_name = await AsyncStorage.getItem("last_name");
-            const first_name = await AsyncStorage.getItem("first_name");
-            getOrders(token);
-            setAvatar(avatar_user);
-            setFirstName(first_name);
-            setLastName(last_name);
-            setId(id_user);
-            setToken(token);
-          } catch (err) {
-            console.log(err);
-          }
+            try {
+                const token = await AsyncStorage.getItem("access_token");
+                const id_user = await AsyncStorage.getItem("id");
+                const avatar_user = await AsyncStorage.getItem("avatar");
+                const last_name = await AsyncStorage.getItem("last_name");
+                const first_name = await AsyncStorage.getItem("first_name");
+                getOrders(token);
+                setAvatar(avatar_user);
+                setFirstName(first_name);
+                setLastName(last_name);
+                setId(id_user);
+                setToken(token);
+            } catch (err) {
+                console.log(err);
+            }
         }
         getToken();
-      }, [render]);
+    }, [render]);
     return (
         <LinearGradient colors={['#edf8f1', '#f7f9fc']} style={styles.linearGradient}>
             <Loader loading={loading} />
             <ScrollView>
                 <View style={{ padding: 10, paddingTop: 10 }}>
-                {
-                        orders.length?
-                            orders.map((item,index)=>{
-                                if((item.status==1)&&(item.user_id==id)){
-                                    return(
+                    {
+                        orders.length ?
+                            orders.map((item, index) => {
+                                if ((item.status == 1) && (item.user_id == id)) {
+                                    return (
                                         <View style={{ marginBottom: 10, padding: 10, backgroundColor: "white", borderColor: "rgb(227, 235, 241)", borderWidth: 1, borderRadius: 5, paddingBottom: 16, boxShadow: "rgb(95 125 149 / 20%) 0px 4px 13px 0px" }}>
-                                        <View style={{ alignItems: "center", flexDirection: "row", marginBottom: 20 }}>
-                                            <View style={{ width: "20%" }}>
-                                            {item.avatar?
-                                                                (item.avatar.search('https://') !== -1)?
-                                                                <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: item.avatar }} />
-                                                                 :
-                                                                    <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: REACT_APP_FILE+'/avatar/'+item.avatar }} />
-                                                                       :
-                                                          <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: REACT_APP_FILE+'/avatar/avatar.png' }} />
-                                                            }
+                                            <View style={{ alignItems: "center", flexDirection: "row", marginBottom: 20 }}>
+                                                <View style={{ width: "20%" }}>
+                                                    {item.avatar ?
+                                                        (item.avatar.search('https://') !== -1) ?
+                                                            <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: item.avatar }} />
+                                                            :
+                                                            <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: REACT_APP_FILE + '/avatar/' + item.avatar }} />
+                                                        :
+                                                        <Avatar.Image size={45} style={{ backgroundColor: "#edf8f1" }} source={{ uri: REACT_APP_FILE + '/avatar/avatar.png' }} />
+                                                    }
+                                                </View>
+                                                <View style={{ width: "80%" }}>
+                                                    <Text style={{ fontSize: 16, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>{item.last_name + " " + item.first_name}<Text style={{ fontSize: 16, fontWeight: "normal" }}> đã đổi phần thưởng </Text>{item.present_name ? item.present_name : null}</Text>
+                                                    <Text style={{ fontSize: 12, color: "rgb(35, 54, 78)" }}>{moment(item.updated_at).fromNow()}</Text>
+                                                </View>
                                             </View>
-                                            <View style={{ width: "80%" }}>
-                                                <Text style={{ fontSize: 16, color: "rgb(35, 54, 78)", fontWeight: "bold" }}>{item.last_name+" "+item.first_name}<Text style={{ fontSize: 16, fontWeight: "normal" }}> đã đổi phần thưởng </Text>{item.present_name?item.present_name:null}</Text>
-                                                <Text style={{ fontSize: 12, color: "rgb(35, 54, 78)" }}>{moment(item.updated_at).fromNow()}</Text>
+                                            <View style={{ alignItems: "center", marginBottom: 20 }}>
+                                                <Image
+                                                    source={{ uri: REACT_APP_FILE + '/present/image/' + item.present_image }}
+                                                    style={{
+                                                        width: '100%',
+                                                        height: 100,
+                                                        resizeMode: 'contain',
+                                                    }}
+                                                />
+                                            </View>
+                                            <View style={{ alignItems: "center", marginBottom: 10 }}>
+                                                <Text style={{ fontSize: 24, fontWeight: "bold", color: "red" }}>{item.present_score ? item.present_score : null} Points</Text>
+                                            </View>
+                                            <View style={{ marginBottom: 10 }}>
+                                                <Text style={{ fontSize: 14, fontWeight: "italic", color: "rgb(35, 54, 78)" }}>Please confirm the employee's redemption information .</Text>
+                                            </View>
+                                            <View style={{ alignItems: "center", marginTop: 10 }}>
+                                                <TouchableOpacity
+                                                    activeOpacity={0.5}
+                                                    style={{
+                                                        borderWidth: 1,
+                                                        borderRadius: 3,
+                                                        borderColor: "#ff9900",
+                                                        backgroundColor: "#FFFF66",
+                                                        width: "90%",
+                                                        padding: 10,
+                                                        alignItems: "center"
+                                                    }}
+                                                    onPress={() => onConfirmEmployee(item.id)}
+                                                >
+                                                    <Text style={{ color: "#ff9900", fontWeight: "bold" }}>CONFIRM RECEIPT</Text>
+                                                </TouchableOpacity>
                                             </View>
                                         </View>
-                                        <View style={{ alignItems: "center", marginBottom: 20 }}>
-                                            <Image
-                                                source={{ uri: REACT_APP_FILE+'/present/image/'+item.present_image }}
-                                                style={{
-                                                    width: '100%',
-                                                    height: 100,
-                                                    resizeMode: 'contain',
-                                                }}
-                                            />
-                                        </View>
-                                        <View style={{ alignItems: "center", marginBottom: 10 }}>
-                                            <Text style={{ fontSize: 24, fontWeight: "bold", color: "red" }}>{item.present_score?item.present_score:null} Points</Text>
-                                        </View>
-                                        <View style={{ marginBottom: 10 }}>
-                                            <Text style={{ fontSize: 14, fontWeight: "italic", color: "rgb(35, 54, 78)" }}>Please confirm the employee's redemption information .</Text>
-                                        </View>
-                                        <View style={{ alignItems: "center", marginTop: 10 }}>
-                                            <TouchableOpacity
-                                                activeOpacity={0.5}
-                                                style={{
-                                                    borderWidth: 1,
-                                                    borderRadius: 3,
-                                                    borderColor: "#ff9900",
-                                                    backgroundColor: "#FFFF66",
-                                                    width: "90%",
-                                                    padding: 10,
-                                                    alignItems: "center"
-                                                }}
-                                                 onPress={()=>onConfirmEmployee(item.id)}
-                                            >
-                                                <Text style={{ color: "#ff9900", fontWeight: "bold" }}>CONFIRM RECEIPT</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                    </View>
-                        )}})
-                        :null
+                                    )
+                                }
+                            })
+                            : null
                     }
                 </View>
             </ScrollView>
